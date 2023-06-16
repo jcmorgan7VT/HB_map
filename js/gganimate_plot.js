459581b2c6b3ecd7e6c68f45112104817f54daae
jcmorgan7VT <113623508+jcmorgan7VT@users.noreply.github.com>
2023-06-16 19:03:23 -0400

    Add files via upload

---
+++ js/gganimate_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#gganimate_plot').scianimator({
+	    'images': ['images/gganimate_plot1.png', 'images/gganimate_plot2.png', 'images/gganimate_plot3.png', 'images/gganimate_plot4.png', 'images/gganimate_plot5.png', 'images/gganimate_plot6.png', 'images/gganimate_plot7.png', 'images/gganimate_plot8.png', 'images/gganimate_plot9.png', 'images/gganimate_plot10.png', 'images/gganimate_plot11.png', 'images/gganimate_plot12.png', 'images/gganimate_plot13.png', 'images/gganimate_plot14.png', 'images/gganimate_plot15.png', 'images/gganimate_plot16.png', 'images/gganimate_plot17.png', 'images/gganimate_plot18.png', 'images/gganimate_plot19.png', 'images/gganimate_plot20.png', 'images/gganimate_plot21.png', 'images/gganimate_plot22.png', 'images/gganimate_plot23.png', 'images/gganimate_plot24.png', 'images/gganimate_plot25.png', 'images/gganimate_plot26.png', 'images/gganimate_plot27.png', 'images/gganimate_plot28.png', 'images/gganimate_plot29.png', 'images/gganimate_plot30.png', 'images/gganimate_plot31.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#gganimate_plot').scianimator('play');
+    });
+})(jQuery);
